f4532216afe10ce6b154161efc64fd96e4b4dd8f
Vladimir <voronov.volodya2013@yandex.ru>
2019-11-17 01:18:37 +0700

    add tests for golomb seq

---
+++ tests/test_sequences.py
@@ -0,0 +1,16 @@
+import sequences
+
+def test_golomb_seq():
+    assert sequences.golomb_seq(1) == [1]
+
+
+def test_golomb_seq_lst():
+    assert sequences.golomb_seq_lst(1) == [1]
+    assert sequences.golomb_seq_lst(2) == [1, 2]
+    assert sequences.golomb_seq_lst(3) == [1, 2, 2]
+
+def test_golomb():
+    n = 10**3
+    lst = sequences.golomb_seq_lst(n)
+    for i, item in enumerate(lst):
+        assert item == sequences.golomb_seq(i)
